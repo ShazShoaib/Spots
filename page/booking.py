75d0475f6bd289ad0ad3e80a51c6b6ec18a5e383
Shaz Shoaib <74443336+ShazShoaib@users.noreply.github.com>
2025-02-26 22:02:39 +0500

    Add files via upload

--- page/booking.py
+++ page/booking.py
@@ -25,13 +25,13 @@ def booking_page():
 
     cols = st.columns([0.33,0.33,0.33])
     with cols[0]:
-        st.selectbox("Party", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        party_size = st.selectbox("Party", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     with cols[1]:
-        st.date_input("Date")
+        party_date = st.date_input("Date")
     with cols[2]:
-        st.selectbox("Time", ["12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"])
+        party_time = st.selectbox("Time", ["12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM"])
 
 
     if st.button("Book Now"):
-        st.write("Booked Successfully")
+        st.write(f"Table Booked for {party_size} on {party_date} at {party_time}")
 
